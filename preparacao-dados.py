2d8b9ce5f1724fea7061d6483021626c7ed6a007
Tainan Rodrigues <tainansilvasci@gmail.com>
2024-06-29 17:36:12 -0300

    criacao de projeto e inicio de preparacao de dados

--- preparacao-dados.py
+++ preparacao-dados.py
@@ -7,9 +7,13 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 data = pd.read_csv('hotel_bookings.csv')
 
 # 1. Tratar valores faltantes
-missing_data_columns = ['agent', 'company']
+missing_data_columns = [
+    'agent',
+    'company'
+]
 for col in missing_data_columns:
-    data[col].fillna(data[col].median())
+    if data[col].dtype in ['float64', 'int64']:
+        data[col] = data[col].fillna(data[col].median())
 
 # Label Encoding para variáveis categóricas de baixa cardinalidade e ordem natural
 le = LabelEncoder()
@@ -33,49 +37,43 @@ for col in le_columns:
 # 2. Tratar outliers com Winsorização
 winso_columns = [
     'lead_time',
-    'required_car_parking_spaces',
-    'arrival_date_year',
-    'arrival_date_month',
-    'arrival_date_week_number',
-    'meal',
-    'country',
-    'market_segment',
-    'reserved_room_type',
-    'assigned_room_type',
-    'customer_type',
-    'reservation_status'
 ]
 
 for col in winso_columns:
     data[col] = winsorize(data[col], limits=[0.05, 0.05])
 
 columns_to_normalize = [
+    'hotel',
+    'is_canceled',
     'lead_time',
-    'required_car_parking_spaces',
     'arrival_date_year',
     'arrival_date_month',
     'arrival_date_week_number',
-    'meal',
-    'country',
-    'market_segment',
-    'reserved_room_type',
-    'assigned_room_type',
-    'customer_type',
-    'reservation_status',
-    'lead_time',
-    'arrival_date_year',
+    'arrival_date_day_of_month',
     'stays_in_weekend_nights',
     'stays_in_week_nights',
     'adults',
     'children',
     'babies',
+    'meal',
+    'country',
+    'market_segment',
+    'distribution_channel',
+    'is_repeated_guest',
     'previous_cancellations',
     'previous_bookings_not_canceled',
+    'reserved_room_type',
+    'assigned_room_type',
     'booking_changes',
-    'required_car_parking_spaces',
+    'deposit_type',
+    'agent',
+    'company',
     'days_in_waiting_list',
+    'customer_type',
     'adr',
-    'total_of_special_requests'
+    'required_car_parking_spaces',
+    'total_of_special_requests',
+    'reservation_status'
 ]
 
 # Criar e aplicar o StandardScaler
