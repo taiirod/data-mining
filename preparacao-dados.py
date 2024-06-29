152f3c29def80c75c2e703310386d520784611bf
Tainan Rodrigues <tainansilvasci@gmail.com>
2024-06-29 17:28:14 -0300

    criacao de projeto e inicio de preparacao de dados

--- preparacao-dados.py
+++ preparacao-dados.py
@@ -11,10 +11,6 @@ missing_data_columns = ['agent', 'company']
 for col in missing_data_columns:
     data[col].fillna(data[col].median())
 
-# 2. Tratar outliers com Winsorização
-data['lead_time'] = winsorize(data['lead_time'], limits=[0.05, 0.05])
-data['required_car_parking_spaces'] = winsorize(data['required_car_parking_spaces'], limits=[0.05, 0.05])
-
 # Label Encoding para variáveis categóricas de baixa cardinalidade e ordem natural
 le = LabelEncoder()
 le_columns = ['hotel',
@@ -34,8 +30,40 @@ le_columns = ['hotel',
 for col in le_columns:
     data[col] = le.fit_transform(data[col])
 
+# 2. Tratar outliers com Winsorização
+winso_columns = [
+    'lead_time',
+    'required_car_parking_spaces',
+    'arrival_date_year',
+    'arrival_date_month',
+    'arrival_date_week_number',
+    'meal',
+    'country',
+    'market_segment',
+    'reserved_room_type',
+    'assigned_room_type',
+    'customer_type',
+    'reservation_status'
+]
+
+for col in winso_columns:
+    data[col] = winsorize(data[col], limits=[0.05, 0.05])
+
 columns_to_normalize = [
     'lead_time',
+    'required_car_parking_spaces',
+    'arrival_date_year',
+    'arrival_date_month',
+    'arrival_date_week_number',
+    'meal',
+    'country',
+    'market_segment',
+    'reserved_room_type',
+    'assigned_room_type',
+    'customer_type',
+    'reservation_status',
+    'lead_time',
+    'arrival_date_year',
     'stays_in_weekend_nights',
     'stays_in_week_nights',
     'adults',
