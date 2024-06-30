b97fc4a88bfae9063051cd6aa08017a744d3056e
Tainan Rodrigues <tainansilvasci@gmail.com>
2024-06-30 12:19:56 -0300

    corrigindo colunas utilizadas em preparação de dados

--- preparacao-dados.py
+++ preparacao-dados.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from scipy.stats.mstats import winsorize
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 
 # Carregar os dados
 data = pd.read_csv('hotel_bookings.csv')
 
 # Drop de colunas não necessarias para o dominio da aplicação
-drop_columns = [
+columns_to_drop = [
     'arrival_date_year',
     'arrival_date_week_number',
     'meal',
@@ -18,7 +20,7 @@ drop_columns = [
     'reservation_status_date'
 ]
 
-data = data.drop(data.columns[[drop_columns]], axis=1)
+data.drop(columns_to_drop, axis=1, inplace=True)
 
 # Tratar valores faltantes
 missing_data_columns = [
@@ -34,25 +36,39 @@ le = LabelEncoder()
 le_columns = [
     'hotel',
     'arrival_date_month',
-    'market_segment'
+    'market_segment',
     'distribution_channel',
     'reserved_room_type',
     'assigned_room_type',
     'deposit_type',
 ]
 
-# Aplicar Label Encoding a cada coluna
 for col in le_columns:
     data[col] = le.fit_transform(data[col])
 
-# 2. Tratar outliers com Winsorização
-winso_columns = [
-    'lead_time',
-]
+# Buscar por colunas que possuem outliers
 
+outliers_count = {}
+winso_columns = []
+for column in data.columns:
+    if data[column].dtype != 'object':  # Exclui colunas não numéricas
+        Q1 = data[column].quantile(0.25)
+        Q3 = data[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - (1.5 * IQR)
+        upper_bound = Q3 + (1.5 * IQR)
+        # Encontra outliers e conta o total
+        outliers = data[(data[column] < lower_bound) | (data[column] > upper_bound)]
+        outliers_count[column] = outliers.shape[0]
+        # Adiciona a coluna à lista se tiver outliers
+        if outliers_count[column] > 0:
+            winso_columns.append(column)
+
+# Tratar outliers com Winsorização
 for col in winso_columns:
     data[col] = winsorize(data[col], limits=[0.05, 0.05])
 
+# Normalizar colunas
 columns_to_normalize = [
     'hotel',
     'lead_time',
@@ -78,7 +94,6 @@ columns_to_normalize = [
     'total_of_special_requests'
 ]
 
-# Criar e aplicar o StandardScaler
 scaler = StandardScaler()
 data[columns_to_normalize] = scaler.fit_transform(data[columns_to_normalize])
 
